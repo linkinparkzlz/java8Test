d2b4a46c63ed94520e69afbc244af3ca9c88118a
zoulvzhou <940933073@qq.com>
2018-02-23 17:57:47 +0800

    Predicate接口实例

---
+++ src/main/java/com/zou/jk8/PredicateTest.java
@@ -0,0 +1,42 @@
+package com.zou.jk8;
+
+import java.util.function.Predicate;
+
+public class PredicateTest {
+
+    public static void main(String[] args) {
+
+        Predicate<String> predicate = p -> p.length() > 5;
+
+        System.out.println(predicate.test("hello"));
+    }
+
+
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+

19fb16075af697c491acf0596531939328428be0
zoulvzhou <940933073@qq.com>
2018-02-25 12:28:42 +0800

    flatMap()举例2

---
+++ src/main/java/com/zou/jk8/StreamTest12.java
@@ -0,0 +1,59 @@
+package com.zou.jk8;
+
+import java.util.Arrays;
+import java.util.List;
+import java.util.stream.Collectors;
+
+public class StreamTest12 {
+
+    public static void main(String[] args) {
+
+        List<String> list1 = Arrays.asList("Hi", "Hello", "你好");
+
+        List<String> list2 = Arrays.asList("zhangsan", "lisi", "wangwu", "zhaoliu");
+
+        //list1和list2进行一一组合
+
+        List<String> result = list1.stream().flatMap(item -> list2.stream().map(item2 -> item + " " + item2)).collect(Collectors.toList());
+
+        result.forEach(System.out::println);
+
+
+    }
+
+
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
